ce5885826a7328b7647712f68529ffc5e17ce198
haasye <luanahaas004@gmail.com>
2026-05-24 17:42:08 -0300

    Adiciona viewsets de alocacao

--- apps/alocacao/views.py
+++ apps/alocacao/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Alocacao, HistoricoAlocacao
+from .serializers import AlocacaoSerializer, HistoricoAlocacaoSerializer
+
+
+class AlocacaoViewSet(viewsets.ModelViewSet):
+    queryset = Alocacao.objects.select_related('solicitacao').all()
+    serializer_class = AlocacaoSerializer
+
+
+class HistoricoAlocacaoViewSet(viewsets.ModelViewSet):
+    queryset = HistoricoAlocacao.objects.select_related('alocacao').all()
+    serializer_class = HistoricoAlocacaoSerializer
