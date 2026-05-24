76356c09af2935a06c4fa52e849ebe267d25e465
Rafael Magnus de Lima Cerutti <111919049+RafaelCerutti@users.noreply.github.com>
2026-05-24 16:23:01 -0300

    urls de locais
    criacao das urls de locais

---
+++ apps/locais/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import (
+    LocalListView,
+    LocalDetailView,
+    LocalCreateView,
+    LocalUpdateView,
+    LocalDeleteView,
+)
+
+
+app_name = 'locais'
+
+
+urlpatterns = [
+    path('', LocalListView.as_view(), name='lista'),
+    path('novo/', LocalCreateView.as_view(), name='criar'),
+    path('<int:pk>/', LocalDetailView.as_view(), name='detalhe'),
+    path('<int:pk>/editar/', LocalUpdateView.as_view(), name='editar'),
+    path('<int:pk>/excluir/', LocalDeleteView.as_view(), name='excluir'),
+]
