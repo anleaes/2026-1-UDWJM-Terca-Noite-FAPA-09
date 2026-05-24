45131fa700e2ca065c7f823be9523172f39b91bf
Rafael Magnus de Lima Cerutti <111919049+RafaelCerutti@users.noreply.github.com>
2026-05-24 12:44:03 -0300

    Update admin.py
    Update do admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,14 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
-
-from .models import (
-    Cliente,
-    Motorista
-)
-
+from .models import Cliente, Funcionario
 
 admin.site.register(Cliente)
-admin.site.register(Motorista)
+admin.site.register(Funcionario)
