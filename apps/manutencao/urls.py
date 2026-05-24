1222d27e35a15c841b98dd07b75617b83cf95ab2
haasye <luanahaas004@gmail.com>
2026-05-24 15:50:43 -0300

    Cria urls de manutencao

---
+++ apps/manutencao/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import ManutencaoViewSet
+
+
+app_name = 'manutencao'
+
+router = DefaultRouter()
+router.register('manutencoes', ManutencaoViewSet, basename='manutencao')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
