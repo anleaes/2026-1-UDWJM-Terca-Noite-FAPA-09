de61a27bdc5383e7f4941f2c74712eb568a87938
GabrielMatos236 <bielbrown2001@hotmail.com>
2026-05-28 21:26:38 -0300

    correção
    correção

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,44 +2,80 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from rest_framework import viewsets
 
-from .forms import GrupoVeiculoForm
-from .models import GrupoVeiculo
-from .serializers import GrupoVeiculoSerializer
+from .forms import ClienteForm, FuncionarioForm
+from .models import Cliente, Funcionario
+from .serializers import ClienteSerializer, FuncionarioSerializer
 
 
-class GrupoVeiculoViewSet(viewsets.ModelViewSet):
-    queryset = GrupoVeiculo.objects.all().order_by('nome')
-    serializer_class = GrupoVeiculoSerializer
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all().order_by('nome')
+    serializer_class = ClienteSerializer
 
 
-class GrupoVeiculoListView(ListView):
-    model = GrupoVeiculo
-    template_name = 'grupo_veiculo/lista.html'
-    context_object_name = 'grupos'
+class FuncionarioViewSet(viewsets.ModelViewSet):
+    queryset = Funcionario.objects.all().order_by('nome')
+    serializer_class = FuncionarioSerializer
 
 
-class GrupoVeiculoDetailView(DetailView):
-    model = GrupoVeiculo
-    template_name = 'grupo_veiculo/detalhe.html'
-    context_object_name = 'grupo'
+class ClienteListView(ListView):
+    model = Cliente
+    template_name = 'users/clientes/lista.html'
+    context_object_name = 'clientes'
 
 
-class GrupoVeiculoCreateView(CreateView):
-    model = GrupoVeiculo
-    form_class = GrupoVeiculoForm
-    template_name = 'grupo_veiculo/form.html'
-    success_url = reverse_lazy('grupo_veiculo_web:lista')
+class ClienteDetailView(DetailView):
+    model = Cliente
+    template_name = 'users/clientes/detalhe.html'
+    context_object_name = 'cliente'
 
 
-class GrupoVeiculoUpdateView(UpdateView):
-    model = GrupoVeiculo
-    form_class = GrupoVeiculoForm
-    template_name = 'grupo_veiculo/form.html'
-    success_url = reverse_lazy('grupo_veiculo_web:lista')
+class ClienteCreateView(CreateView):
+    model = Cliente
+    form_class = ClienteForm
+    template_name = 'users/clientes/form.html'
+    success_url = reverse_lazy('users_web:clientes_lista')
 
 
-class GrupoVeiculoDeleteView(DeleteView):
-    model = GrupoVeiculo
-    template_name = 'grupo_veiculo/confirmar_delete.html'
-    success_url = reverse_lazy('grupo_veiculo_web:lista')
-    context_object_name = 'grupo'
+class ClienteUpdateView(UpdateView):
+    model = Cliente
+    form_class = ClienteForm
+    template_name = 'users/clientes/form.html'
+    success_url = reverse_lazy('users_web:clientes_lista')
+
+
+class ClienteDeleteView(DeleteView):
+    model = Cliente
+    template_name = 'users/clientes/confirmar_delete.html'
+    success_url = reverse_lazy('users_web:clientes_lista')
+
+
+class FuncionarioListView(ListView):
+    model = Funcionario
+    template_name = 'users/funcionarios/lista.html'
+    context_object_name = 'funcionarios'
+
+
+class FuncionarioDetailView(DetailView):
+    model = Funcionario
+    template_name = 'users/funcionarios/detalhe.html'
+    context_object_name = 'funcionario'
+
+
+class FuncionarioCreateView(CreateView):
+    model = Funcionario
+    form_class = FuncionarioForm
+    template_name = 'users/funcionarios/form.html'
+    success_url = reverse_lazy('users_web:funcionarios_lista')
+
+
+class FuncionarioUpdateView(UpdateView):
+    model = Funcionario
+    form_class = FuncionarioForm
+    template_name = 'users/funcionarios/form.html'
+    success_url = reverse_lazy('users_web:funcionarios_lista')
+
+
+class FuncionarioDeleteView(DeleteView):
+    model = Funcionario
+    template_name = 'users/funcionarios/confirmar_delete.html'
+    success_url = reverse_lazy('users_web:funcionarios_lista')
